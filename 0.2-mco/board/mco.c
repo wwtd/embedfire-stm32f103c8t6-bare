fe1b2a8d675b3ca60bb5496b71eff7aa0c34f10a
wwtd <51043256+wwtd@users.noreply.github.com>
2024-08-24 17:25:18 +0800

    add mco select func

--- 0.2-mco/board/mco.c
+++ 0.2-mco/board/mco.c
@@ -3,15 +3,41 @@
 #include "gpio.h"
 #include <stdint.h>
 
-#define RCC_BASE   0x40021000
-#define RCC_CFGR   (*((volatile uint32_t *)(RCC_BASE + 0x04)))
-#define GPIOA_BASE 0x40010800
-#define GPIOA_CRH  (*((volatile uint32_t *)(GPIOA_BASE + 0x04)))
-
 void mco_init()
 {
-    set_gpio_crl_crh(GPIOA_CTRL_BLOCK_ADDR, PA8, GPIO_CNF_AF_OUT_PUSH_PULL, GPIO_MODE_OUT_SPPED_50MHZ);
+    set_gpio_crl_crh(GPIOA_CTRL_BLOCK_ADDR, PA8, GPIO_CNF_AF_OUT_PUSH_PULL,
+                     GPIO_MODE_OUT_SPPED_50MHZ);
+    mco_select_mode(HSE_OUTPUT);
+}
 
-    RCC_CFGR &= ~(0x7 << 24);
-    RCC_CFGR |= (0x4 << 24);
+void mco_select_mode(E_MCO_OUTPUT_MODE output_mode)
+{
+    SRccControlBlock *rcc_ptr = RCC_CONTROL_BLOCK_ADDR;
+    rcc_ptr->cfg &= ~(0x7 << 24);
+
+    switch (output_mode)
+    {
+        case NO_CLOCK_OUTPUT: {
+            rcc_ptr->cfg |= (MCO_NO_OUTPUT_MASK << 24);
+            break;
+        }
+        case SYSCLK_OUTPUT: {
+            rcc_ptr->cfg |= (MCO_SYSCLK_OUTPUT_MASK << 24);
+            break;
+        }
+        case HSI_OUTPUT: {
+            rcc_ptr->cfg |= (MCO_HSI_OUTPUT_MASK << 24);
+            break;
+        }
+        case HSE_OUTPUT: {
+            rcc_ptr->cfg |= (MCO_HSE_OUTPUT_MASK << 24);
+            break;
+        }
+        case HALF_PLL_OUTPUT: {
+            rcc_ptr->cfg |= (MCO_HALF_PLL_OUTPUT_MASK << 24);
+            break;
+        }
+        default:
+            break;
+    }
 }
--- 0.2-mco/board/mco.h
+++ 0.2-mco/board/mco.h
@@ -2,6 +2,23 @@
 #ifndef _BOARD_INCLUDE_MCO_H_
 #define _BOARD_INCLUDE_MCO_H_
 
+typedef enum
+{
+    NO_CLOCK_OUTPUT = 0x1,
+    SYSCLK_OUTPUT   = 0x2,
+    HSI_OUTPUT      = 0x3,
+    HSE_OUTPUT      = 0x4,
+    HALF_PLL_OUTPUT = 0x5,
+} E_MCO_OUTPUT_MODE;
+
+#define MCO_NO_OUTPUT_MASK       (0b000)
+#define MCO_SYSCLK_OUTPUT_MASK   (0b100)
+#define MCO_HSI_OUTPUT_MASK      (0b101)
+#define MCO_HSE_OUTPUT_MASK      (0b110)
+#define MCO_HALF_PLL_OUTPUT_MASK (0b111)
+
 void mco_init();
+
+void mco_select_mode(E_MCO_OUTPUT_MODE output_mode);
 
 #endif //_BOARD_INCLUDE_MCO_H_
