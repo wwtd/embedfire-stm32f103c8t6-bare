aa99c6bfc28480b308c3df61f52785466a915963
wwtd <51043256+wwtd@users.noreply.github.com>
2024-09-02 22:03:32 +0800

    add a format print example

--- 03-uart/app/main.c
+++ 03-uart/app/main.c
@@ -2,18 +2,21 @@
 #include "board.h"
 #include "on-board-led.h"
 #include "log.h"
+#include <stdint.h>
 
 int main(int argc, char *argv[])
 {
     board_init();
+    uint32_t loop_count = 0;
     for (;;)
     {
         on_board_led_turn_on(FIRST);
-        log_console_print(LOG_LEVEL_DEBUG, "on");
+        log_console_print(LOG_LEVEL_DEBUG, "[%u]on", loop_count);
         delay(1000);
         on_board_led_turn_off(FIRST);
-        log_console_print(LOG_LEVEL_DEBUG, "off");
+        log_console_print(LOG_LEVEL_DEBUG, "[%u]off", loop_count);
         delay(1000);
+        loop_count++;
     }
     return 0;
 }
